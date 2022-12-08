eabfd337e65265dd543df51dfa6358824a1f6d6a
LoisChen68 <a8283626@gmail.com>
2022-12-09 06:45:54 +0800

    style: add desktop header & refactor header scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,64 +10,66 @@ export default function Header() {
   return (
     <>
       <header>
-        <div className={style['header-container']}>
-          <a className={style['header-logo-container']} href="#">
-            <img className={style['icon-logo']} src={Logo} />
-          </a>
-          <div className={style['navbar-burger-warper']}>
-            <label
-              htmlFor="navbar-toggle"
-              className={style['burger-container']}
-            >
-              <img src={Vector} className={style['icon-toggle']} />
-            </label>
+        <div className={style.container}>
+          <div className={style['navbar-container']}>
+            <a className={style['logo-wrapper']} href="#">
+              <img className={style['icon-logo']} src={Logo} />
+            </a>
+            <div className={style['navbar-burger-wrapper']}>
+              <label
+                htmlFor="navbar-toggle"
+                className={style['burger-container']}
+              >
+                <img src={Vector} className={style['icon-toggle']} />
+              </label>
+            </div>
+            <input
+              id="navbar-toggle"
+              className={style['navbar-toggle']}
+              type="checkbox"
+            />
+            <nav className={style['navbar-menu']}>
+              <ul className={style['nav-list-link']}>
+                <li className={style['nav-item__link']}>
+                  <a className={style['nav-link']} href="#">
+                    男款
+                  </a>
+                </li>
+                <li className={style['nav-item__link']}>
+                  <a className={style['nav-link']} href="#">
+                    女款
+                  </a>
+                </li>
+                <li className={style['nav-item__link']}>
+                  <a className={style['nav-link']} href="#">
+                    最新消息
+                  </a>
+                </li>
+                <li className={style['nav-item__link']}>
+                  <a className={style['nav-link']} href="#">
+                    客製商品
+                  </a>
+                </li>
+                <li className={style['nav-item__link']}>
+                  <a className={style['nav-link']} href="#">
+                    聯絡我們
+                  </a>
+                </li>
+              </ul>
+              <ul className={style['nav-list-icon']}>
+                <li className={style['nav-item__icon']}>
+                  <img src={Search} className={style['nav-icon-search']}></img>
+                </li>
+                <li className={style['nav-item__icon']}>
+                  <img src={Cart} className={style['nav-icon-cart']} />
+                </li>
+                <li id="theme-toggle" className={style['nav-item__icon']}>
+                  <img src={Moon} className={style['nav-icon-moon']} />
+                  <img src={Sun} className={style['nav-icon-sun']} />
+                </li>
+              </ul>
+            </nav>
           </div>
-          <input
-            id="navbar-toggle"
-            className={style['navbar-toggle']}
-            type="checkbox"
-          />
-          <nav className={style['navbar-menu']}>
-            <ul className={style['nav-list-text-link']}>
-              <li className={style['nav-item__link']}>
-                <a className={style['nav-link']} href="#">
-                  男款
-                </a>
-              </li>
-              <li className={style['nav-item__link']}>
-                <a className={style['nav-link']} href="#">
-                  女款
-                </a>
-              </li>
-              <li className={style['nav-item__link']}>
-                <a className={style['nav-link']} href="#">
-                  最新消息
-                </a>
-              </li>
-              <li className={style['nav-item__link']}>
-                <a className={style['nav-link']} href="#">
-                  客製商品
-                </a>
-              </li>
-              <li className={style['nav-item__link']}>
-                <a className={style['nav-link']} href="#">
-                  聯絡我們
-                </a>
-              </li>
-            </ul>
-            <ul className={style['nav-list-icon']}>
-              <li className={style['nav-item__icon']}>
-                <img src={Search} className={style['nav-icon-search']}></img>
-              </li>
-              <li className={style['nav-item__icon']}>
-                <img src={Cart} className={style['nav-icon-cart']} />
-              </li>
-              <li id="theme-toggle" className={style['nav-item__icon']}>
-                <img src={Moon} className={style['nav-icon-moon']} />
-                <img src={Sun} className={style['nav-icon-sun']} />
-              </li>
-            </ul>
-          </nav>
         </div>
       </header>
     </>
